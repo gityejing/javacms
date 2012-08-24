b313fff0e420ce060e7a526cc1448f2876e7fca9
andy.zhao <83849377@qq.com>
2012-08-24 23:51:48 +0800

    Add LoginFilter.java

---
+++ src/com/idceye/servlet/LoginFilter.java
@@ -0,0 +1,37 @@
+package com.idceye.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginFilter implements Filter {
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse resp,
+			FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp; 
+		
+		System.out.println(request.getRequestURI());
+		chain.doFilter(request, response);
+
+	}
+
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
